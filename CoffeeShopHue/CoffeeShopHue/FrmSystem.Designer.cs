0dacf0008d19322fbae614160ba02c06893b4f69
huanhoang2121 <38126411+huanhoang2121@users.noreply.github.com>
2018-04-11 14:44:05 +0700

    # CoffeeShop Project

---
+++ CoffeeShopHue/CoffeeShopHue/FrmSystem.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CoffeeShopHue
+{
+    partial class FrmSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuSystem = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.systemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accountManagerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loggingSystemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSystem.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuSystem
+            // 
+            this.MenuSystem.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.systemToolStripMenuItem});
+            this.MenuSystem.Location = new System.Drawing.Point(0, 0);
+            this.MenuSystem.Name = "MenuSystem";
+            this.MenuSystem.Size = new System.Drawing.Size(800, 24);
+            this.MenuSystem.TabIndex = 0;
+            this.MenuSystem.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // systemToolStripMenuItem
+            // 
+            this.systemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginToolStripMenuItem,
+            this.logoutToolStripMenuItem,
+            this.accountManagerToolStripMenuItem,
+            this.loggingSystemToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.systemToolStripMenuItem.Name = "systemToolStripMenuItem";
+            this.systemToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.systemToolStripMenuItem.Text = "System";
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // accountManagerToolStripMenuItem
+            // 
+            this.accountManagerToolStripMenuItem.Name = "accountManagerToolStripMenuItem";
+            this.accountManagerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.accountManagerToolStripMenuItem.Text = "Account Manager";
+            // 
+            // loggingSystemToolStripMenuItem
+            // 
+            this.loggingSystemToolStripMenuItem.Name = "loggingSystemToolStripMenuItem";
+            this.loggingSystemToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loggingSystemToolStripMenuItem.Text = "Logging System";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // FrmSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.MenuSystem);
+            this.MainMenuStrip = this.MenuSystem;
+            this.Name = "FrmSystem";
+            this.Text = "CoffeeShop";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MenuSystem.ResumeLayout(false);
+            this.MenuSystem.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuSystem;
+        private System.Windows.Forms.ToolStripMenuItem systemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accountManagerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loggingSystemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
